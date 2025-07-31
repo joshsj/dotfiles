e006247c806b5daf5a3e2dcdacc64e290c1e6c89
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2025-07-31 13:00:49 +0100

    Change terminal font, remove default LSP servers

--- AppData/Local/nvim/lua/joshsj/lazy/lspconfig.lua
+++ AppData/Local/nvim/lua/joshsj/lazy/lspconfig.lua
@@ -162,8 +162,6 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      'zls',
-      'roslyn',
       'lua-language-server',
       'vim-language-server',
       volar = {},
